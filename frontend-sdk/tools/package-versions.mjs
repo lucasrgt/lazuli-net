2f4439b4decb329c244738bbf8ae5ec3b5bd321d
Lucas Tinoco <lucas.tinoco@hotmail.com>
2026-08-11 14:27:47 -0300

    adopt assay design 0.5.4

--- frontend-sdk/tools/package-versions.mjs
+++ frontend-sdk/tools/package-versions.mjs
@@ -2,8 +2,8 @@
 // This file ships inside @skiesjs/frontend-sdk, making the sync check independent of a sibling
 // framework checkout and therefore effective in CI as well as on a developer machine.
 export const FRONTEND_PACKAGE_VERSIONS = Object.freeze([
-  Object.freeze({ name: "@skiesjs/frontend-sdk", version: "4.1.6" }),
-  Object.freeze({ name: "assay-design", version: "0.4.13" }),
+  Object.freeze({ name: "@skiesjs/frontend-sdk", version: "4.1.7" }),
+  Object.freeze({ name: "assay-design", version: "0.5.4" }),
   Object.freeze({ name: "avp-assay", version: "0.4.0" }),
   Object.freeze({ name: "@skiesjs/react", version: "4.0.5" }),
   Object.freeze({ name: "@skiesjs/eslint-plugin", version: "4.0.5" }),
